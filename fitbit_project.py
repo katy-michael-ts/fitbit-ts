6516a8feefa9f232aef255568b0916a2a9e6d707
Michael P. Moran <mmutexas@gmail.com>
2019-04-19 15:59:52 -0500

    add template for project script

---
+++ fitbit_project.py
@@ -0,0 +1,76 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Fitbit Time Series Project
+# By Kathryn Salts & Michael Moran
+
+# ## TODO
+
+# ## Table of contents
+# 1. [Project Planning](#project-planning)
+# 1. [Acquisition](#acquisition)
+# 1. [Preparation](#preparation)
+# 1. [Exploration](#exploration)
+# 1. [Modeling](#modeling)
+
+# ## Project Planning <a name="project-planning"></a>
+
+# ### Goals
+
+# ### Deliverables
+
+# ### Data Dictionary & Domain Knowledge
+
+# ### Hypotheses
+
+# ### Thoughts & Questions
+
+# ### Prepare the Environment
+
+# **Reload modules to capture changes**
+
+# ## Acquisition <a name="acquisition"></a>
+
+# ## Preparation <a name="preparation"></a>
+
+# ### Summarize Data
+
+# ### Handle Missing Values
+
+# ### Handle Duplicates
+
+# ### Fix Data Types
+
+# ### Handle Outliers
+
+# ### Check Missing Values
+
+# ## Exploration  <a name="exploration"></a>
+
+# ### Train-Test Split
+
+# ### Visualizations
+
+# ### Statistical Tests
+
+# ### Summarize Conclusions
+
+# ## Modeling <a name="modeling"></a>
+
+# ### Feature Engineering & Selection
+
+# ### Train & Test Models
+
+# ### Summarize Conclusions
