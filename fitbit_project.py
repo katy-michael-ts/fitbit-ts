4386bb37cc6a2a918572e260fae1b04a58449c7a
Michael P. Moran <mmutexas@gmail.com>
2019-04-24 10:03:51 -0500

    reorganize notebook, document steps taken in preparing data; move acquire and preparation code to py file

--- fitbit_project.py
+++ fitbit_project.py
@@ -51,30 +51,33 @@
 # ### Hypotheses
 #
 # 1. The wearer may be testing fitness trackers.
-# 1. The wearer appears to be fairly active, but not mostly mobile activity, possibly stationary like lifting weights.
-# 1. Probably not someone in a drug trial because there are not many entries in the food log. We would expect them to be logging food to see if there are any interactions.
+# 1. The wearer is fairly active, but not mostly mobile activity, possibly stationary like lifting weights.
+# 1. Wearer is not someone in a drug trial because there are not many entries in the food log. We would expect them to be logging food to see if there are any interactions.
 # 1. The wearer is likely not wearing the tracker while sleeping. Average inactivity/activity minutes is 16-17 hours per day.
 #     - Wouldn't expect a person testing fitness equipment to wear it that long
 #     - Would make sense for an employee or drug trial participant (but we would expect the drug trial participant to wear it while sleeping)
 # 1. Likely not a person testing fitness equipment because there are food log entries for one week; and also likely not a drug trial participant for the same reason
 #     - Makes it more likely to be an employee who lost the motivation to log food
-# - Next two week
 # 1. Looks like person stopped wearing tracker on 12/7/18 because there are food log entries and caloric intake entries for dates after the 7th, but the activity log stops on the 6th.
 
 # ### Thoughts & Questions
 #
 # 1. What does the weekend data look like? This may tell us whether they work there or are in a drug trial  (likely to wear the fitbit on the weekend) or are testing fitness equipment (not likely to wear on weekend)
+# 1. Why does the activity tracking data end on 12/7 but the food log and caloric intake log keep going?
+# 1. What do we do with the food log entries?
+#     - Got rid of them for now because it doesn't appear to be worth the effort to wrangle them
 
 # ### Prepare the Environment
 
 # +
-import os
 from pprint import pprint
-from enum import Enum, auto
 import io
 from datetime import timedelta
+from importlib import reload
 
 import adalib
+import acquire
+import prepare
 
 import pandas as pd
 
@@ -83,61 +86,14 @@ import pandas as pd
 
 # **Reload modules to capture changes**
 
+adalib = reload(adalib)
+acquire = reload(acquire)
+prepare = reload(prepare)
+
 # ## Acquisition <a name="acquisition"></a>
 
 
-class CSVType(Enum):
-    CALS_IN = auto()
-    ACTIVITIES = auto()
-    FOOD_LOG = auto()
-
-
-# +
-DATADIR = "fitbit"
-
-CALS_IN_HEADER = "Date,Calories In"
-ACTIVITIES_HEADER = "Date,Calories Burned,Steps,Distance,Floors,Minutes Sedentary,Minutes Lightly Active,Minutes Fairly Active,Minutes Very Active,Activity Calories"
-FOOD_LOG_HEADER = "Daily Totals"
-
-cals_in = []
-activities = []
-food_log = []
-for file in sorted(os.listdir(DATADIR)):
-    with open(DATADIR + "/" + file) as fp:
-        lines = fp.readlines()
-        csvtype = None
-        date = ""
-        for line in lines:
-            # START OF NEW CSV
-            if line.startswith("Foods"):
-                csvtype = CSVType.CALS_IN
-            elif line.startswith("Activities"):
-                csvtype = CSVType.ACTIVITIES
-            elif line.startswith("Food Log"):
-                toks = line.split()
-                date = toks[2]
-                csvtype = CSVType.FOOD_LOG
-
-            # START OF HEADER
-            elif (
-                line.startswith(CALS_IN_HEADER)
-                or line.startswith(ACTIVITIES_HEADER)
-                or line.startswith(FOOD_LOG_HEADER)
-            ):
-                continue
-
-            # IT'S DATA!
-            elif csvtype == CSVType.CALS_IN:
-                cals_in.append(line)
-            elif csvtype == CSVType.ACTIVITIES:
-                activities.append(line)
-            elif csvtype == CSVType.FOOD_LOG:
-                if line.startswith('""'):
-                    line = line.replace(
-                        '""', f'"{date[:4]}-{date[4:6]}-{date[6:]}"'
-                    )
-                food_log.append(line)
-
+df_cals_in, df_activities, df_food_log = acquire.acquire_fitbit()
 
 # +
 # pprint(cals_in)
@@ -153,161 +109,100 @@ for file in sorted(os.listdir(DATADIR)):
 
 # ### Prepare Caloric Intake Dataframe
 
-cals_in_cols = ["date", "calories_in"]
-df_cals_in = pd.read_csv(
-    io.StringIO("".join(cals_in)), header=None, names=cals_in_cols
-)
+# 1. Remove columns from calories_in and convert to int type
+# 1. Convert the date column to datetime type
 
+df_cals_in = prepare.df_cals_in_prepare(df_cals_in)
 
-# +
-def df_col_to_datetime(df: pd.DataFrame, col: str, **kwargs) -> pd.DataFrame:
-    datetimed = pd.to_datetime(df[col], **kwargs)
-    out = df.copy()
-    out[col] = datetimed
-    return out
-
-
-def series_remove_commas(series: pd.Series) -> pd.Series:
-    return series.str.replace(",", "")
-
-
-def df_cals_in_prepare(df: pd.DataFrame) -> pd.DataFrame:
-    df["calories_in"] = series_remove_commas(df.calories_in)
-    df = df_col_to_datetime(df_cals_in, "date", format="%Y-%m-%d")
-    return df.astype({"calories_in": int})
-
-
-# -
-
-df_cals_in = df_cals_in_prepare(df_cals_in)
-
-
-def has_every_day(date_series: pd.Series) -> bool:
-    return timedelta(date_series.nunique()) == (
-        date_series.max() - date_series.min()
-    ) + timedelta(days=1)
-
-
-has_every_day(df_cals_in.date)
+prepare.has_every_day(df_cals_in.date)
 
 df_cals_in.info()
 
 # ### Prepare Activities Dataframe
 
-activities_cols = [
-    "date",
-    "calories_burned",
-    "steps",
-    "distance",
-    "floors",
-    "minutes_sedentary",
-    "minutes_lightly_active",
-    "minutes_fairly_active",
-    "minutes_very_active",
-    "activity_calories",
-]
-df_activities = pd.read_csv(
-    io.StringIO("".join(activities)), header=None, names=activities_cols
-)
+# 1. Remove commas from calories_burned, steps, minutes_sedentary, activity calories and convert those columns to int type
+# 1. Convert date column to datetime type
 
+df_activities = prepare.df_activities_prepare(df_activities)
 
-def df_activities_prepare(df: pd.DataFrame) -> pd.DataFrame:
-    df = df_col_to_datetime(df, "date", format="%Y-%m-%d")
-
-    df["calories_burned"] = series_remove_commas(df.calories_burned)
-    df["steps"] = series_remove_commas(df.steps)
-    df["minutes_sedentary"] = series_remove_commas(df.minutes_sedentary)
-    df["activity_calories"] = series_remove_commas(df.activity_calories)
-
-    return df.astype(
-        {
-            "calories_burned": int,
-            "steps": int,
-            "minutes_sedentary": int,
-            "activity_calories": int,
-        }
-    )
-
-
-df_activities = df_activities_prepare(df_activities)
-
-has_every_day(df_activities.date)
+prepare.has_every_day(df_activities.date)
 
 df_activities.info()
 
 # ### Prepare Food Log Dataframe
 
-# food_log_cols = ["date", "calories", "fat", "fiber", "carbs", "sodium",
-#                  "protein", "water"]
-food_log_cols = ["date", "column", "value"]
-df_food_log = pd.read_csv(
-    io.StringIO("".join(food_log)), header=None, names=food_log_cols
-)
+# 1. Grab only the data corresponding to the nutritional stats, not the specific food entries, which do not appear to be worth the effort
+# 1. Reset the index, rename column to date and convert to datetime type
+
+df_food_log, _ = prepare.df_food_log_prepare(df_food_log)
+
+prepare.has_every_day(df_food_log.date)
 
 df_food_log.info()
 
-# **I need to iterate over the DataFrame, use the date as the index, make columns for calories, fat, etc and put the value there**
+# ### Merge DataFrames, Sort, and Index by Date
 
-# +
-columns = ("calories", "fat", "fiber", "carbs", "sodium", "protein", "water")
-out_of_place = []
-out = pd.DataFrame()
-for index, row in df_food_log.iterrows():
-    if isinstance(row["column"], str):
-        col_lower = row["column"].lower()
-        if col_lower in columns:
-            out.loc[row["date"], col_lower] = row["value"]
-        else:
-            out_of_place.append(row)
-    else:
-        out_of_place.append(row)
+# 1. Merge the three dataframes on their date column using an outer join
+# 1. Drop the columns calories, fat, fiber, carbs, sodium, protein, water because they contain fewer than 10 non-zero entries.
 
-#     print(row["column"], row["value"])
-# -
-
-out = out.reset_index()
-out = out.rename(columns={"index": "date"})
-
-
-# +
-def df_food_log_prepare(df: pd.DataFrame) -> pd.DataFrame:
-    df = df_col_to_datetime(df, "date", format="%Y-%m-%d")
-    return df
-
-
-#     df["calories_burned"] = series_remove_commas(df.calories_burned)
-#     df["steps"] = series_remove_commas(df.steps)
-#     df["minutes_sedentary"] = series_remove_commas(df.minutes_sedentary)
-#     df["activity_calories"] = series_remove_commas(df.activity_calories)
-
-#     return df.astype({"calories_burned": int, "steps": int, "minutes_sedentary": int, "activity_calories": int})
-
-
-# -
-
-out = df_food_log_prepare(out)
-
-out.info()
-
-# ### Start Merging Dataframes
-
-df = df_cals_in.merge(df_activities, how="outer", on="date")
+df = prepare.prepare_fitbit(df_cals_in, df_activities, df_food_log)
 
 df.info()
 
-df = df.merge(out, how="outer", on="date")
+# ### Summarize Data
 
-df = df.sort_values("date").set_index("date")
+adalib.summarize(df)
 
-# ### What to do with the extras? <a name="todo-extra-data"></a>
+# ### Handle Missing Values
 
-print(out_of_place[0])
-print()
-print(out_of_place[1])
-print()
-print(out_of_place[2])
+df.isnull().sum()
 
-df.describe()
+adalib.df_missing_vals_by_col(df)
+
+df_bad_rows = adalib.df_missing_vals_by_row(df)
+df_bad_rows[(df_bad_rows.nmissing > 0) | (df_bad_rows.nempty > 0)]
+
+# **Drop the missing rows**
+
+df = df.dropna()
+
+adalib.df_missing_vals_by_col(df)
+
+# ### Handle Duplicates
+
+# ### Fix Data Types
+
+# ### Handle Outliers
+
+# ### Check Missing Values
+
+# ### Summarize Data
+
+# **Thoughts**
+# - What is distance measured in?
+# - What does "floors" mean?
+# - What is the difference between the "calories in" and "calories" columns?
+# -
+#
+# **Conclusions**
+#
+# 1. calories_in has a mean of 51; looks like it is mostly 0
+# 1. The mean of calories_buned appears to be above the average for a man and way above average for a woman
+# 1. Steps and distance metrics appear to match up
+# 1. This person is sedentary for on average 13+ hours
+# 1. There appear to be days where the Fitbit was not worn or not worn much. min of calories_burned is 799. min of steps and distance and floors is 0. minutes sedentary is 1440 (24 hours).
+# 1. columns to drop
+#     - calories_in (239 rows are 0)
+# 1. After looking at the binned data, it looks like this person was active much of the time by looking at calories_burned and steps.
+# 1.
+
+pd.concat([df.head(14), df.tail(14)])
+
+# ## Exploration  <a name="exploration"></a>
+
+# ### Train-Test Split
+
+# ### Visualizations
 
 # +
 import matplotlib.pyplot as plt
@@ -367,7 +262,7 @@ for i, col in enumerate(
 
 df["calories_burned_bin"] = pd.qcut(df.calories_burned, q=4)
 
-df.date.min()
+df.index.min()
 
 sns.swarmplot(
     x="calories_burned_bin", y="activity_calories", data=df, palette="Set2"
@@ -386,66 +281,6 @@ train = df[:"2018-08"]
 test = df["2018-12":]
 print(train.nunique())
 print(test.nunique())
-
-# ### Summarize Data
-
-# ### Handle Missing Values
-
-df.isnull().sum()
-
-df[df.isnull().any(axis=1)]
-
-# **Impute calories_burned using the mean**
-
-# **Impute steps by mean**
-
-# **Impute distance by mean**
-
-# **Impute floors with mean**
-
-#
-
-#
-
-# ### Handle Duplicates
-
-# ### Fix Data Types
-
-# ### Handle Outliers
-
-# ### Check Missing Values
-
-# ### Summarize Data
-
-df.info()
-
-adalib.summarize(df)
-
-# **Thoughts**
-# - What is distance measured in?
-# - What does "floors" mean?
-# - What is the difference between the "calories in" and "calories" columns?
-# -
-#
-# **Conclusions**
-#
-# 1. calories_in has a mean of 51; looks like it is mostly 0
-# 1. The mean of calories_buned appears to be above the average for a man and way above average for a woman
-# 1. Steps and distance metrics appear to match up
-# 1. This person is sedentary for on average 13+ hours
-# 1. There appear to be days where the Fitbit was not worn or not worn much. min of calories_burned is 799. min of steps and distance and floors is 0. minutes sedentary is 1440 (24 hours).
-# 1. columns to drop
-#     - calories_in (239 rows are 0)
-# 1. After looking at the binned data, it looks like this person was active much of the time by looking at calories_burned and steps.
-# 1.
-
-pd.concat([df.head(14), df.tail(14)])
-
-# ## Exploration  <a name="exploration"></a>
-
-# ### Train-Test Split
-
-# ### Visualizations
 
 # ### Statistical Tests
 
